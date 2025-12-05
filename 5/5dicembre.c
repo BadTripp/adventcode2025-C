38934b601ae1217316982c9ee1b17fb696023e0a
BadTripp <renato.turetta00@gmail.com>
2025-12-05 15:07:25 +0100

    2/2 works on example - performance issue on puzzle input

--- 5/5dicembre.c
+++ 5/5dicembre.c
@@ -2,22 +2,66 @@
 #include<string.h>
 #include<stdlib.h>
 int sum = 0;
-int sumRange = 0;
+char sumRange[256] ="0" ;
 typedef struct {
 	long long int primo;
 	long long int secondo;
 } Coppia;
+int sumRangeString(){
+	int len = strlen(sumRange)-1;	
+	while(len >= 0){
+		if(sumRange[len] < '9'){
+			sumRange[len]++;
+			return 1;
+		}
+		if(sumRange[len] == '9'){
+			if(len == 0){
+				sumRange[len] = '0';
+				memmove(sumRange+1,sumRange,strlen(sumRange)+1);	
+				sumRange[0]= '1'; 
+			}else{
+				sumRange[len] = '0';
+			}
+			
+				
+		}
+		
+		len--;
+	}
+}
+
 void countRange(Coppia *range,int dim){
 	for(int i = 0; i < dim;i++){
 		for(long long j = range[i].primo; j <= range[i].secondo;j++){
-			sumRange++;	
+			//printf("%s\n",sumRange);	
+			int check = 0;
+			for(long long k = i-1 ; k >= 0 ; k--){
+				// se presente nei range  precedenti
+				if(j >= range[k].primo && j <= range[k].secondo){
+					check = 1;	
+					printf("%lld \n",j);
+				}	
+
+			}	
+			if(check == 0){
+				printf("\033[H\033[J");
+				printf("totale range %d\n",dim);
+				printf("range %d\n",i);
+				printf("%s\n",sumRange);
+			  	sumRangeString();
+			}
+			
+				
+			
+				
+			//printf("contatore%d\n",j);		
 			}
 	}
 }
 int checkId(long long int ingr,Coppia *range,int dim){
 		for(int i = 0; i < dim;i++){
 			if(ingr >= range[i].primo && ingr <= range[i].secondo){	
-				printf("Sommo %d , %ld ,%ld\n",ingr,range[i].primo,range[i].secondo);
+				//printf("Sommo %d , %ld ,%ld\n",ingr,range[i].primo,range[i].secondo);
 				sum++;
 				return 1;	
 			}
@@ -63,15 +107,15 @@ int  main (void){
 			
 			sscanf(line,"%lld",&num);
 			checkId(num,range,lc);		
-			printf("number %lld\n",num);
+			//printf("number %lld\n",num);
 			}
 						
 	}
 	for(int i = 0; i < lc;i++){
-		printf("Range : %d,%d\n",range[i].primo,range[i].secondo);
+		//printf("Range : %lld,%lld\n",range[i].primo,range[i].secondo);
 		}
 	countRange(range,lc);
-	printf("sum : %d  sumRange : %lld \n",sum,sumRange);
+	printf("sum : %d  sumRange : %s \n",sum,sumRange);
 
 			
 	return 1;
